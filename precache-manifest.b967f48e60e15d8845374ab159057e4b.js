abad4e4bbfe575e36984b02244b081ba722ab967
Christopher Hacker <christopher.hacker@student.tugraz.at>
2020-10-14 09:31:15 +0200

    Updates

---
+++ precache-manifest.b967f48e60e15d8845374ab159057e4b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9ddad19f4f823c28d567d93d64d0bd",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "7fc44b9c16926d17ff29",
+    "url": "/vidly/static/css/2.66afcd11.chunk.css"
+  },
+  {
+    "revision": "521a7ef3087a53647c0e",
+    "url": "/vidly/static/css/main.5e160cba.chunk.css"
+  },
+  {
+    "revision": "7fc44b9c16926d17ff29",
+    "url": "/vidly/static/js/2.190feb9a.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/vidly/static/js/2.190feb9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521a7ef3087a53647c0e",
+    "url": "/vidly/static/js/main.ecdebf5d.chunk.js"
+  },
+  {
+    "revision": "cb388922157c702ba0d5",
+    "url": "/vidly/static/js/runtime-main.f8720289.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
