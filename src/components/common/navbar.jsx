321fd63e4453bcea3d1191ffd5b69d2925ed4439
Christopher Hacker <christopher.hacker@student.tugraz.at>
2020-10-15 14:00:18 +0200

    fixed somme routing issues

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -5,7 +5,7 @@ class Navbar extends Component {
     render() { 
         return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Vidly</Link>
+        <Link className="navbar-brand" to="/vidly">Vidly</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
